903c76fbea988488c8676611275b2b6e34381572
Tudor73 <tudorminciunescu@gmail.com>
2021-09-23 21:10:56 +0300

    Revert "refactoring"
    This reverts commit 9836e02230a5d58bc049e5df003c68fcf44b61fb.

--- server/index.js
+++ server/index.js
@@ -21,6 +21,13 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 const PORT = process.env.PORT || 3000
 
+// configure redis for storing session 
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient({
+    port: 6379,
+    host: 'localhost'
+});
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors());
@@ -33,6 +40,7 @@ app.use(
         secret: 'keyboard cat',
         resave: false,
         saveUninitialized: false,
+        store: new RedisStore({ client: redisClient }),
         cookie: {
             secure: false, // if true: only transmit cookies with https
             httpOnly: true, // prevents client side JS from reading cookie
@@ -40,6 +48,17 @@ app.use(
         }
     })
 )
+// passport config  
+passportConfig(passport);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+    console.log(req.passport);
+    next();
+})
+
 app.use('/auth', authRoutes);
 app.use('/posts', postRoutes);
 app.use('/comments', commentRoutes);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,13 @@
 import express from "express";
+import passport from "passport";
 
 const router = express.Router();
+
+router.get('/google', passport.authenticate('google', { scope: ['profile'], successFlash: "Welcome" }));
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: 'http://localhost:3001/error' }), (req, res) => {
+    console.log(req.sessionID);
+    res.redirect('http://localhost:3001/posts');
+});
 
 router.get('/logout', (req, res) => {
     if (req.session) {
